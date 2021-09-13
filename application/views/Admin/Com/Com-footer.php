cf53679dd2a9481d0a4f1fda1a3e6b3ffa61a7b1
irfal31@gmail.com <Kalinjuang123>
2021-09-13 20:06:44 +0700

    first

--- application/views/Admin/Com/Com-footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-    </div>
-    <strong>Copyright &copy; <?php print  "2019"?> <?php  print $projectname ?></strong> All rights reserved.
-  </footer>
-</body>
-</html>
