af234db5bf8e68062d60ca5a11cb20a8d6edac0d
irfal nafiyandi <irfal31@gmail.com>
2021-10-27 09:31:44 +0700

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -368,7 +368,7 @@ class Home extends CI_Controller {
 
 			$mail = new PHPMailer(true);
 			$mail->isSMTP();
-			#$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+			$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
 			$mail->Host       = 'smtp.googlemail.com';
 			$mail->SMTPAuth   = true;
